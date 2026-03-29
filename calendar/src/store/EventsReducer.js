a6a6c08c4d495ce5e37a766f17fd67bf596b5ed1
BSenichak <bsenk77@gmail.com>
2026-03-29 15:49:56 +0300

    sdfsdf

--- calendar/src/store/EventsReducer.js
+++ calendar/src/store/EventsReducer.js
@@ -1,19 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    events: []
-}
+    events: [],
+    loading: false,
+    error: null
+};
 
 const EventsReducer = createSlice({
-  name: "events",
-  initialState,
-  reducers: {
-    addEvent: (state, action) => {
-        state.events = [...state.events, action.payload]
+    name: "events",
+    initialState,
+    reducers: {
+        addEvent: (state, action) => {
+            state.events = [...state.events, action.payload];
+        },
+    },
+    extraReducers: (builder)=> {
+      builder.addCase(addNewEvent.pending, (state)=>{
+        state.loading = true
+        state.error = null
+      })
+      builder.addCase(addNewEvent.rejected, (state, action)=>{
+        state.loading = false
+        state.error = action.error.message
+      })
+      builder.addCase(addNewEvent.fulfilled, (state, action)=> {
+        state.loading = false
+        state.events = action.payload
+      })
     }
-  }
 });
 
-export const {addEvent} = EventsReducer.actions
+export const addNewEvent = createAsyncThunk(
+    "events/addNewEvent",
+    async (data, { getState, rejectWithValue }) => {
+        try {
+            const response = await fetch("http://localhost:3000/add", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${getState().auth.token}`,
+                },
+                body: JSON.stringify(data),
+            });
+            let result = await response.json();
+            if (!response.ok) {
+                throw rejectWithValue({ error: result.error });
+            }
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    },
+);
 
-export default EventsReducer.reducer
+export const { addEvent } = EventsReducer.actions;
+
+export default EventsReducer.reducer;
